c1023f89c23a9319941c50dbd0f9b55f06b15ab0
lhy <liuhongyu2017@aliyun.com>
2020-08-19 21:49:57 +0800

    爬虫

--- src/main/java/com/study/crawler/Demo.java
+++ src/main/java/com/study/crawler/Demo.java
@@ -7,6 +7,23 @@ package com.study.crawler;
 public class Demo {
 
   public static void main(String[] args) {
-    System.out.println("Hello,World!");
+    System.out.print(randomNum(33, 1));
+    System.out.print("  ");
+    System.out.print(randomNum(33, 1));
+    System.out.print("  ");
+    System.out.print(randomNum(33, 1));
+    System.out.print("  ");
+    System.out.print(randomNum(33, 1));
+    System.out.print("  ");
+    System.out.print(randomNum(33, 1));
+    System.out.print("  ");
+    System.out.print(randomNum(33, 1));
+    System.out.print("   ");
+    System.out.print(randomNum(16, 1));
   }
+
+  public static int randomNum(int max, int min) {
+    return (int) (Math.random() * (max - min) + min);
+  }
+
 }
